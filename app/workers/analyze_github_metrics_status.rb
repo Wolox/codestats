ef98fbcdf1bac7ef121ab826cc3f212533a75436
Esteban Pintos <esteban.pintos@wolox.com.ar>
2016-04-26 11:19:33 -0300

    Refactor in analyze github metrics status worker

--- app/workers/analyze_github_metrics_status.rb
+++ app/workers/analyze_github_metrics_status.rb
@@ -2,6 +2,7 @@ class AnalyzeGithubMetricsStatus
   include Sidekiq::Worker
   include Rails.application.routes.url_helpers
   sidekiq_options retry: false
+  attr_reader :pull_request, :github_service
 
   def perform(pull_request_data)
     @pull_request = GithubPullRequest.new(pull_request_data)
@@ -9,26 +10,26 @@ class AnalyzeGithubMetricsStatus
   end
 
   def handle_pull_request_status
-    @project = Project.find_by(github_repo: @pull_request.full_name)
-    return unless @project.present?
-    @branch = @project.branches.find_by(name: @pull_request.branch)
-    return unless @branch.present?
-    @github_service = GithubService.new(@project.admin_user)
-    change_pull_request_status(pull_request_status)
+    project = Project.find_by(github_repo: pull_request.full_name)
+    return unless project.present?
+    branch = project.branches.find_by(name: pull_request.branch)
+    return unless branch.present?
+    @github_service = GithubService.new(project.admin_user)
+    change_pull_request_status(pull_request_status(project, branch))
   end
 
-  def change_pull_request_status(status)
-    @github_service.create_status(
-      @pull_request, status[:key],
+  def change_pull_request_status(_project, status)
+    github_service.create_status(
+      pull_request, status[:key],
       context: 'CodeStats',
       # TODO: Add host
-      # target_url: organization_project_branch_url(@project.organization, @project, @branch),
+      # target_url: organization_project_branch_url(project.organization, @project, @branch),
       description: status[:description]
     )
   end
 
-  def pull_request_status
-    BranchManager.new(@branch).metrics_status_success? ? success : failure
+  def pull_request_status(branch)
+    BranchManager.new(branch).metrics_status_success? ? success : failure
   end
 
   def success
