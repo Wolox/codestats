4686399655cac667d404c6cbabc5b25314ca2a8b
Esteban Pintos <esteban.pintos@wolox.com.ar>
2016-07-04 23:35:34 -0300

    Adds codestats report gem

--- app/models/github_pull_request.rb
+++ app/models/github_pull_request.rb
@@ -2,8 +2,8 @@ class GithubPullRequest
   attr_reader :full_name, :sha, :branch
 
   def initialize(pull_request_data)
-    @full_name = pull_request_data[:full_name]
-    @sha = pull_request_data[:sha]
-    @branch = pull_request_data[:branch]
+    @full_name = pull_request_data['full_name']
+    @sha = pull_request_data['sha']
+    @branch = pull_request_data['branch']
   end
 end
