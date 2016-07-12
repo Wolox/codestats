3f9b0e30a239a6e89e94b4e79122e43a7c965255
Matias De Santi <mdesanti90@gmail.com>
2016-07-12 14:51:38 -0300

    Adds minimum to chart

--- app/formatter/metric_chart_data_formatter.rb
+++ app/formatter/metric_chart_data_formatter.rb
@@ -3,7 +3,8 @@ class MetricChartDataFormatter
     def format(metrics)
       {
         labels: metrics.map(&:created_at).map { |a| a.strftime('%Y/%m/%d %H:%M:%S') },
-        values: metrics.map(&:value)
+        values: metrics.map(&:value),
+        minimum: metrics.map(&:minimum).map(&:to_f)
       }
     end
   end
