e281fbc2971f3fad32de2d2fba0b34c514b59727
Matias De Santi <mdesanti90@gmail.com>
2016-07-13 13:30:06 -0300

    Adds error badge

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,4 +1,11 @@
+# frozen_string_literal: true
 class BadgesController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :error_badge
+
+  ERROR_BADGE = 'badge-error-lightgrey.svg'
+  SUCCESS_BADGE = 'checks-passed-green.svg'
+  FAIL_BADGE = 'checks-failed-red.svg'
+
   def index
     branch = organization.projects.friendly.find(params[:id]).default_branch
     file = get_badge_for_branch(branch)
@@ -7,12 +14,22 @@ class BadgesController < ApplicationController
 
   private
 
+  def error_badge
+    file = asset_path(ERROR_BADGE)
+    send_data open(file).read, type: 'image/svg+xml;charset=utf-8', disposition: :inline
+  end
+
   def get_badge_for_branch(branch)
+    return asset_path(ERROR_BADGE) if branch.nil?
     if BranchManager.new(branch).metrics_status_success?
-      Rails.root.join('public', 'checks-passed-green.svg')
+      asset_path(SUCCESS_BADGE)
     else
-      Rails.root.join('public', 'checks-failed-red.svg')
+      asset_path(FAIL_BADGE)
     end
+  end
+
+  def asset_path(name)
+    Rails.root.join('public', name)
   end
 
   def organization
