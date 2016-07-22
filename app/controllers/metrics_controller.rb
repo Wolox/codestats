06c3fb16063c0d922f71ed61bea25f05965670c1
Matias De Santi <mdesanti90@gmail.com>
2016-07-22 16:20:53 -0300

    Adds metric history limit

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,7 +1,9 @@
 class MetricsController < ApplicationController
+  METRIC_HISTORY_LIMIT = 20
+
   def chart_data
     return render :bad_request if params[:metric_name].nil?
-    metrics = branch.metrics.where(name: params[:metric_name]).order('created_at asc')
+    metrics = metrics_for(branch)
     authorize metrics.first
     render json: MetricChartDataFormatter.format(metrics)
   end
@@ -18,5 +20,10 @@ class MetricsController < ApplicationController
 
   def organization
     @organization ||= Organization.friendly.find(params[:organization_id])
+  end
+
+  def metrics_for(branch)
+    branch.metrics.where(name: params[:metric_name]).order('created_at asc')
+          .limit(METRIC_HISTORY_LIMIT)
   end
 end
